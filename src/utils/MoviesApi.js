2eb22f9008f8516017477da4967c4a10360fb828
TarStaBor <TarStaBor@yandex.ru>
2022-01-13 14:48:45 +0300

    fix: clear MoviesApi

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -14,105 +14,10 @@ export class MoviesApi {
   getListCard() {
     return fetch(this._url, {
       method: "GET",
-      //   headers: {
-      //     authorization: `Bearer ${localStorage.getItem('token')}`,
-      //     'Content-Type': 'application/json'
-      //   }
     }).then((response) => {
       return this._getResponseData(response);
     });
   }
-
-  // получить список всех карточек в виде массива (GET)
-  // getListCard() {
-  //   return fetch(this._url + "/cards", {
-  //     method: "GET",
-  //     headers: {
-  //       authorization: `Bearer ${localStorage.getItem('token')}`,
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }).then((response) => {
-  //     return this._getResponseData(response);
-  //   });
-  // }
-
-  //   // добавить карточку (POST)
-  //   postCard(text) {
-  //     return fetch(this._url + "/cards", {
-  //       method: "POST",
-  //       headers: {
-  //         authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(text),
-  //     }).then((response) => {
-  //       return this._getResponseData(response);
-  //     });
-  //   }
-  //   // удалить карточку (DELETE)
-  //   deleteCard(id) {
-  //     return fetch(this._url + "/cards/" + id, {
-  //       method: "DELETE",
-  //       headers: {
-  //         authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //     }).then((response) => {
-  //       return this._getResponseData(response);
-  //     });
-  //   }
-  //   // получить данные пользователя (GET)
-  //   getUserInfo() {
-  //     return fetch(this._url + "/users/me", {
-  //       method: "GET",
-  //       headers: {
-  //         authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //     }).then((response) => {
-  //       return this._getResponseData(response);
-  //     });
-  //   }
-
-  //   // заменить данные пользователя (PATCH)
-  //   patchUserInfo(text) {
-  //     return fetch(this._url + "/users/me", {
-  //       method: "PATCH",
-  //       headers: {
-  //         authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(text),
-  //     }).then((response) => {
-  //       return this._getResponseData(response);
-  //     });
-  //   }
-  //   // заменить аватар (PATCH)
-  //   patchAvatar(url) {
-  //     return fetch(this._url + "/users/me/avatar", {
-  //       method: "PATCH",
-  //       headers: {
-  //         authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(url),
-  //     }).then((response) => {
-  //       return this._getResponseData(response);
-  //     });
-  //   }
-
-  //   // “изменить” статус лайка (PUT/DELETE)
-  //   changeLikeCardStatus(id, isLiked) {
-  //     return fetch(this._url + `/cards/${id}/likes`, {
-  //       method: isLiked ? "PUT" : "DELETE",
-  //       headers: {
-  //         authorization: `Bearer ${localStorage.getItem("token")}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //     }).then((response) => {
-  //       return this._getResponseData(response);
-  //     });
-  //   }
 }
 
 const api = new MoviesApi({
