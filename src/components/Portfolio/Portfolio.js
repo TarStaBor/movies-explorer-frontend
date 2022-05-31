ce4769bec7c85c9bfe906239bb23098c3d7eec93
TarStaBor <TarStaBor@yandex.ru>
2022-05-31 19:16:44 +0300

    fix: update portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -3,17 +3,26 @@ import Arrow from "../../images/arrow.svg";
 
 function Portfolio() {
   return (
-    <section className="portfolio">
+    <section id="portfolio" className="portfolio">
       <div className="portfolio__container">
         <h2 className="portfolio__title">Портфолио</h2>
 
+        <a href="https://choicejobs.ru/" target="_blank" rel="noreferrer" className="portfolio__box link-opacity">
+          <h2 className="portfolio__box-title">
+            Приложение для работы с вакансиями <span className="portfolio__box-span">CHOICE</span>
+          </h2>
+          <img src={Arrow} className="portfolio__box-arrow" alt="Изображение стрелки"></img>
+        </a>
+
         <a
-          href="https://tarstabor.github.io/how-to-learn/"
+          href="https://mesto-frontend.tarstabor.nomoredomains.rocks/"
           target="_blank"
           rel="noreferrer"
           className="portfolio__box link-opacity"
         >
-          <h2 className="portfolio__box-title">Статичный сайт</h2>
+          <h2 className="portfolio__box-title">
+            Одностраничное приложение <span className="portfolio__box-span">MESTO</span>
+          </h2>
           <img src={Arrow} className="portfolio__box-arrow" alt="Изображение стрелки"></img>
         </a>
 
@@ -23,16 +32,21 @@ function Portfolio() {
           rel="noreferrer"
           className="portfolio__box link-opacity"
         >
-          <h2 className="portfolio__box-title">Адаптивный сайт</h2>
+          <h2 className="portfolio__box-title">
+            Адаптивный сайт <span className="portfolio__box-span">RUSSIAN TRAVEL</span>
+          </h2>
           <img src={Arrow} className="portfolio__box-arrow" alt="Изображение стрелки"></img>
         </a>
+
         <a
-          href="https://mesto-frontend.tarstabor.nomoredomains.rocks/"
+          href="https://tarstabor.github.io/how-to-learn/"
           target="_blank"
           rel="noreferrer"
           className="portfolio__box link-opacity"
         >
-          <h2 className="portfolio__box-title">Одностраничное приложение</h2>
+          <h2 className="portfolio__box-title">
+            Статичный сайт <span className="portfolio__box-span">HOW TO LEARN?</span>
+          </h2>
           <img src={Arrow} className="portfolio__box-arrow" alt="Изображение стрелки"></img>
         </a>
       </div>
